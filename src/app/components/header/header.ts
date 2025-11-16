39959ed67ff05f8b22072b9663b794b2532a7141
Nikolai Escondo <nescondo2@gmail.com>
2025-11-15 22:01:52 -0500

    font change and icon

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +10,21 @@ import { MatTabsModule } from '@angular/material/tabs';
     RouterLink,
     RouterLinkActive,
     MatTabsModule,
+    MatIconModule
   ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
+  
+  constructor() {
+    const iconRegistry = inject(MatIconRegistry);
+    const sanitizer = inject(DomSanitizer);
+
+    iconRegistry.addSvgIcon('home', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/home.svg'));
+  }
+
   navLinks = [
     { path: '/', label: 'Home', exact: true },
     { path: '/aboutus', label: 'About Us', exact: false },
