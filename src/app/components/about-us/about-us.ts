57ef9eee9625deb8bba2e4d9b0feaace4cd783fb
Nikolai Escondo <nescondo2@gmail.com>
2025-11-16 01:07:03 -0500

    combined header into home file for single page navigation

--- src/app/components/about-us/about-us.ts
+++ src/app/components/about-us/about-us.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCard } from "@angular/material/card";
 
 @Component({
   selector: 'app-about-us',
-  imports: [],
+  imports: [
+    MatCard
+  ],
   templateUrl: './about-us.html',
   styleUrl: './about-us.scss'
 })
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { ViewportScroller } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-home',
   imports: [
     MatCardModule,
     MatButtonModule,
+    MatIconModule,
+    MatTabsModule,
 ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
 
+  constructor(private viewportScroller: ViewportScroller) {}
+
+  scrollToSection(sectionId: string): void {
+    this.viewportScroller.scrollToAnchor(sectionId);
+  }
 }
